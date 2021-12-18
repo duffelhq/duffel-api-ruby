1620b97cd26588c7a4901ad7ee5624001f9d6530
Tim Rogers <tim@duffel.com>
2021-12-18 17:37:32 +0000

    Gem boilerplate generated with `bundle gem duffel_api`

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-12-18
+
+- Initial release
---
+++ lib/duffel_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "duffel_api/version"
+
+module DuffelApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
